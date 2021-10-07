d83eeda98962fd24e44ac88ea3fd1deb5f9fd623
MordoJP <72819682+MordoJP@users.noreply.github.com>
2021-10-07 22:37:07 +0300

    created a login page

--- src/components/Auth/Login.vue
+++ src/components/Auth/Login.vue
@@ -1,17 +1,95 @@
 <template>
-  <v-container>
-    <v-layout row>
-      <v-flex xs12>
-        <h1>Login</h1>
+  <v-container fluid>
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm8 md6>
+        <v-card class="elevation-12">
+          <v-toolbar dark color="primary">
+            <v-toolbar-title>Login form</v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-form
+              ref="form"
+              v-model="valid"
+              validation
+            >
+              <v-text-field
+                prepend-icon="mdi-account"
+                name="email"
+                label="Email"
+                type="email"
+                v-model="email"
+                :error-messages="emailErrors"
+                @input="$v.email.$touch()"
+                @blur="$v.email.$touch()"
+              ></v-text-field>
+              <v-text-field
+                prepend-icon="mdi-key"
+                name="password"
+                label="Password"
+                :type="showPass ? 'text' : 'password'"
+                v-model="password"
+                :append-icon="showPass ? 'mdi-eye' : 'mdi-eye-off'"
+                :rules="[passwordRules.required, passwordRules.min]"
+                @click:append="showPass = !showPass"
+              ></v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="primary"
+              @click="onSubmit"
+              :disabled="!valid"
+            >Login</v-btn>
+          </v-card-actions>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
+import { validationMixin } from 'vuelidate'
+import { required, email } from 'vuelidate/lib/validators'
+
 export default {
+  mixins: [validationMixin],
+
+  validations: {
+    email: { required, email }
+  },
   data () {
-    return {}
+    return {
+      email: '',
+      password: '',
+      valid: false,
+      showPass: false,
+      passwordRules: {
+        required: value => !!value || 'Password is required',
+        min: v => v.length >= 6 || 'Password must be equal or more than 6 characters'
+      }
+    }
+  },
+  computed: {
+    emailErrors () {
+      const errors = []
+      if (!this.$v.email.$dirty) return errors
+      !this.$v.email.email && errors.push('Must be valid e-mail')
+      !this.$v.email.required && errors.push('E-mail is required')
+      return errors
+    }
+  },
+  methods: {
+    onSubmit () {
+      if (this.valid) {
+        const user = {
+          email: this.email,
+          password: this.password
+        }
+
+        console.log(user)
+      }
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -3,11 +3,12 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import vuetify from './plugins/vuetify'
-import '@babel/polyfill'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
+import Vuelidate from 'vuelidate'
 
 Vue.config.productionTip = false
+Vue.use(Vuelidate)
 
 new Vue({
   router,
